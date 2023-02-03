02bfb7ee773f5b6b3f2c26f0754d4737c3f3a1d1
devharsh007 <hs707803@gmail.com>
2023-02-03 22:14:40 +0530

    Setted up algo properly

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ PLEASE HELP I CANT SOLVE IT*/
 
 
 let  buffer = '0';
-const screen = document.querySelector('.calc-scr');
-if (!screen) {
-    console.error('The .calc-scr class element was not found');
-}
-
 //const screen= document.querySelector('.calc-scr'); //it sets the '.calc-scr' class element to screen (its our output screen )
+const screen = document.querySelector('.calc-scr');
+let runningTotal=0;
+let previousOperator=null; // yet to be committed
+
+
 init();
 function init(){  // initilising the main
     document.querySelector('.calc-btn') //selecting every for event buttons 
@@ -17,6 +17,7 @@ function init(){  // initilising the main
         buttonClick(event.target.innerText); // when '.calc-btn' clicks the buttonClick function will be called
     });
 };
+//initilising the function
 function buttonClick(value){    
     if(isNaN(parseInt(value))){ 
         handleSymbol(value); 
@@ -26,22 +27,91 @@ function buttonClick(value){
         handleNumber(value);  
     } // if it's  a number, it will call handleNumber() fun
     rerender();   
-}; //initilising the function
+}; 
 
+//if  buffer is zero, it will set buffer to string and then it will make string as number to add other (tens, hundred, thousands) places in it
 function handleNumber(number){ 
     if(buffer==='0'){
         buffer= number;
     }else{
         buffer += number;
     }
-};//if  buffer is zero, it will set buffer to string and then it will make string as number to add other (tens, hundred, thousands) places in it
-
-function handleSymbol(symbol){
-    buffer += symbol;
-    rerender();
 };
- //algorithm to be added 
- 
+
+//algorithm to be added 
+function handleSymbol(symbol){
+    switch(symbol){     
+        case 'C':       //if c is pressed screen should display '0'
+            buffer='0';
+            break;
+        case '=':
+            equals();   //will inhibit a function
+            break;
+        case '←':
+            backarrow();    //will inhibit a function
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            handleMath(symbol);      //will inhibit a function
+            break;        
+    }
+};
+
+// we will assigning the buffer value to acreen to show the numbers entered
 function rerender(){    
-    screen.value = buffer; 
-}; // we will assigning the buffer value to acreen to show the numbers entered
+    screen.innerText = buffer;
+}; 
+//making function for backarrow
+function backarrow(){
+    if(buffer.length===1){
+        buffer = "0";
+    } else{
+        buffer = buffer.substring(0,buffer.length-1);
+    }
+}
+
+//for handling oprators
+function handleMath(value){
+    if(buffer===0){ 
+        //do nothing
+        return
+    }
+    const intbuffer= parseInt(buffer);  //changing the buffer str into intiger  
+    if(runningTotal===0){
+        runningTotal=intbuffer;        //changing the 0 to int so that opration with can also be done
+    }else{
+        flushOperation(intbuffer);      //calling a function
+    }
+    previousOperator=value;             //....
+    buffer='0';     // setting buffer to 0 so that another operation can also be started
+}   
+function flushOperation(buffer){
+    switch(intbuffer){      //setting the operands for arthmetic operation
+        case "+":   
+        runningTotal+=parseInt(buffer);         
+        break;
+        case '-':
+        runningTotal-=parseInt(buffer);
+        break;
+        case '*':
+        runningTotal*=parseInt(buffer);
+        break;
+        case '/':
+        runningTotal/=parseInt(buffer);
+        break;
+    }
+}
+
+function equals(){
+    if(previousOperator===null){
+        // need two numbers to do math
+        return;
+    }
+    flushOperation(parseInt(buffer));       // doing operation after clicking = sign
+    buffer=""+runningTotal; //"" convert a number into a string cz string + num =str
+    runningTotal=0;         // setting total to 0 again for another opration (reseting all values)
+    previousOperator = null;    // reseting all values
+
+}
