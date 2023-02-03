6bd7aa189653becad4811a88e7ca9e6e1579f2f4
devharsh007 <hs707803@gmail.com>
2023-02-03 19:27:01 +0530

    It looks like the problem is that the screen variable is set to the wrong element. It's set to the .calc-scr class element, but the input element with the .calc-scr class has a type attribute set to text. To fix this, update the JavaScript code to select the input element with the .calc-scr class:
    const screen= document.querySelector('.calc-scr');
    
    Additionally, the value of the input element can be set with the value property instead of the innerText property:
    
    function rerender(){
        screen.value = buffer;
    };

--- index.js
+++ index.js
@@ -4,35 +4,44 @@ PLEASE HELP I CANT SOLVE IT*/
 
 
 let  buffer = '0';
-const screen= document.querySelector('.calc-scr'); //it sets the '.calc-scr' class element to screen (its our output screen )
+const screen = document.querySelector('.calc-scr');
+if (!screen) {
+    console.error('The .calc-scr class element was not found');
+}
+
+//const screen= document.querySelector('.calc-scr'); //it sets the '.calc-scr' class element to screen (its our output screen )
 init();
 function init(){  // initilising the main
     document.querySelector('.calc-btn') //selecting every for event buttons 
-    .addEventListener("click", function (event) { //setting the event of button click to call the a  function 
+    .addEventListener("click", function (event) { //setting the event of button click to call the a  function
         buttonClick(event.target.innerText); // when '.calc-btn' clicks the buttonClick function will be called
     });
 };
-function buttonClick(value){    //initilising the ffunction 
-    if(isNaN(parseInt(value))){ // checking if the value is a symbol or number
-        handleSymbol(value); // if it's not  a number, it will call handleSymbol() fun 
-    }
+function buttonClick(value){    
+    if(isNaN(parseInt(value))){ 
+        handleSymbol(value); 
+    } // if it's not  a number, it will call handleSymbol() fun 
+    // checking if the value is a symbol or number
     else{
-        handleNumber(value); // if it's  a number, it will call handleNumber() fun 
-    }
+        handleNumber(value);  
+    } // if it's  a number, it will call handleNumber() fun
     rerender();   
-};
+}; //initilising the function
 
-function handleNumber(number){ //if  buffer is zero, it will set buffer to string and then it will make string as number to add other (tens, hundred, thousands) places in it
+function handleNumber(number){ 
     if(buffer==='0'){
         buffer= number;
     }else{
         buffer += number;
     }
-};
+};//if  buffer is zero, it will set buffer to string and then it will make string as number to add other (tens, hundred, thousands) places in it
+
 function handleSymbol(symbol){
-    console.log('symbol');
+    buffer += symbol;
+    rerender();
 };
+ //algorithm to be added 
  
-function rerender(){    // we will assigning the buffer value to acreen to show the numbers entered
-    screen.innerText = buffer; 
-};
+function rerender(){    
+    screen.value = buffer; 
+}; // we will assigning the buffer value to acreen to show the numbers entered
